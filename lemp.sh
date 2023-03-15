f73ba76e6534bb576ea559d12fc29155bdc08a92
t <tinh.doan@codecomplete.jp>
2023-03-15 16:32:18 +0700

    update

--- lemp.sh
+++ lemp.sh
@@ -7,7 +7,7 @@ sudo ufw allow http
 sudo ufw reload
 #install php
 sudo apt install -y php8.1-fpm
-sudo apt install -y php8.1-common php8.1-mysql php8.1-cgi php8.1-mbstring php8.1-curl php8.1-gd php8.1-xml php8.1-xmlrpc php8.1-pear
+sudo apt install -y php8.1-common php8.1-mysql php8.1-mysqli php8.1-cgi php8.1-mbstring php8.1-curl php8.1-gd php8.1-xml php8.1-xmlrpc php8.1-pear
 
 #remove defautl config
 cd /etc/nginx/sites-enabled
