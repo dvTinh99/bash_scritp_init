f1b0581966e005ee4baa5e3b66b69d99ba8c95cd
t <tinh.doan@codecomplete.jp>
2023-03-15 16:41:00 +0700

    update

--- lemp.sh
+++ lemp.sh
@@ -7,7 +7,7 @@ sudo ufw allow http
 sudo ufw reload
 #install php
 sudo apt install -y php8.1-fpm
-sudo apt install -y php8.1-common php8.1-mysql php8.1-mysqli php8.1-cgi php8.1-mbstring php8.1-curl php8.1-gd php8.1-xml php8.1-xmlrpc php8.1-pear
+sudo apt install -y php8.1-common php8.1-mysql php8.1-cgi php8.1-mbstring php8.1-curl php8.1-gd php8.1-xml php8.1-xmlrpc php8.1-pear
 
 #remove defautl config
 cd /etc/nginx/sites-enabled
@@ -40,6 +40,7 @@ send \"exit\n\"
 "
 #install phpmyadmin 
 cd /usr/share
+sudo apt install -y php8.1-mysqli
 sudo wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-english.tar.gz
 sudo tar -zxvf phpMyAdmin-5.2.1-english.tar.gz
 
