49c10c009e0107654ee3353ce5ba6d1ba52f4ad0
t <tinh.doan@codecomplete.jp>
2023-03-15 11:11:45 +0700

    update

--- lemp.sh
+++ lemp.sh
@@ -1,6 +1,11 @@
 sudo apt update
 
+#install nginx
 sudo apt install -y nginx
-
+# check firewall
 sudo ufw allow http
 sudo ufw reload
+#install php
+sudo apt install -y php-fpm
+sudo apt install -y php-common php-mysql php-cgi php-mbstring php-curl php-gd php-xml php-xmlrpc php-pear
+
